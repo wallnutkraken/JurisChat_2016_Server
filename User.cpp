3ad4e9e1e464c7234f0565decedbd0a893ebad85
Emile <emilisbliudzius@gmail.com>
2016-05-11 19:36:30 +0200

    added basic user class

--- User.cpp
+++ User.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "User.h"
+
+using namespace std;
+
+class User
+{
+
+public:
+    User::User(Server& serverReference) {
+        server = serverReference;
+    }
+    void User::Disconnect() {
+
+    }
+    bool User::AttemptLogin(std::string username, std::string passwordAttempt) {
+        return false;
+    }
+
+    std::string User::getSessionToken() {
+        return _sessionToken;
+    }
+
+    void User::setSessionToken(std::string newToken) {
+        _sessionToken = newToken;
+    }
+    void User::addUnreadMessage(Message message) {
+        /* Not implemented */
+    }
+    Message* User::getUnreadMessages() { /* returns a pointer to the array _unreadMessages */
+        return _unreadMessages;
+    }
+};
--- User.h
+++ User.h
@@ -5,10 +5,29 @@
 #ifndef JURISCHAT_2016_SERVER_USER_H
 #define JURISCHAT_2016_SERVER_USER_H
 
+#include <string>
+#include "Constants.h"
 
 class User
 {
+    Server server;
 
+    std::string _username;
+    std::string _saltedPasswordHash;
+    std::string _salt;
+
+    std::string _sessionToken;
+    Message _unreadMessages[MAX_UNREAD_MESSAGES_STORED];
+
+public:
+    User(Server& serverReference);
+    void Disconnect();
+    bool AttemptLogin(std::string username, std::string passwordAttempt);
+
+    std::string getSessionToken();
+    void setSessionToken(std::string newToken);
+    void addUnreadMessage(Message message);
+    Message* getUnreadMessages(); /* returns a pointer to the array _unreadMessages */
 };
 
 
